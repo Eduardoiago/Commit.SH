30b1193c325dc7fdfb8da4e6b3e2fffa0e170f64
Eduardoiago <eduardoiago.work@gmail.com>
2024-08-28 00:53:50 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,2 +1,36 @@
-# Commit.SH
-Shell script to optimize commits 
+# Commit.SH 
+![Shell Script](https://img.shields.io/badge/shell_script-%23121011.svg?style=for-the-badge&logo=gnu-bash&logoColor=white)
+
+Shell script to optimize project commits. Using the script automatically you use `git add .`, `git commit -m` and `git push -u origin main`. The main branch is the default. If you are committing to another branch, you should change the branch name.
+
+### Adding execute permission to the script
+
+``` bash
+    chmod +x commit.sh
+```
+
+### Creating alias for the script
+
+- After downloading, move the script to `/opt`
+- Enter the `.bashrc` file and add the alias below:
+
+``` bash
+    alias commit='bash /opt/commit.sh'
+```
+
+## Usage
+
+``` bash
+    commit -h
+```
+
+<img src="/assets/commit-H.png" alt="commit.sh-h"> 
+
+``` bash
+    commit -m "your commit"
+```
+
+<img src="/assets/commit-M.png" alt="commit.sh-m"> 
+
+## License
+ * [MIT](LICENSE)
---
+++ commit.sh
@@ -0,0 +1,79 @@
+#!/bin/bash
+
+# Script name:         COMMIT.SH
+# Author:              Eduardo iago
+# Shell script para otimizar os commits dos projetos, usando o script
+# automaticamente você usa o `git add .`, `git commit -m` e `git push -u origin main`.
+
+USAGE() { # Função para exibir mensagens de uso
+	echo
+	echo "     _____                           _ _         _     "
+	echo "    /  __ \                         (_) |       | |    "
+	echo "    | /  \/ ___  _ __ ___  _ __ ___  _| |_   ___| |__  "
+	echo "    | |    / _ \| '_   _ \| '_   _ \| | __| / __|  _ \ "
+	echo "    | \__/\ (_) | | | | | | | | | | | | |_ _\__ \ | | |"
+	echo "     \____/\___/|_| |_| |_|_| |_| |_|_|\__(_)___/_| |_|"
+	echo "                                 Create by Eduardo Iago"
+	echo
+	echo "         The script adds the modified files using"
+	echo "       git add . , adds your message to the command"
+	echo "        git commit -m (your commit) and pushes it."
+	echo
+	echo "             Usage: ./commit.sh [-m] [message]"
+	echo
+	echo "      Options:"
+	echo
+	echo "   -m | --message       Add the changes, add commit and push"
+	echo "   -h | --help          Usage message"
+}
+
+# Verificar se nenhum argumento foi usado
+if [ $# -eq 0 ]; then
+	echo "[-] Error: No parameters supplied."
+    	USAGE
+    	exit 1
+fi
+
+# Processando os parâmetros
+while [[ "$#" -gt 0 ]]; do
+    	case $1 in
+        	-m|--message)
+            		if [ -n "$2" ] && [ "${2:0:1}" != "-" ]; then
+                		MESSAGE="$2"
+                		shift
+            		else
+                		echo "[-] Error: The -m | --message parameter requires an argument"
+                		exit 1
+            		fi
+            		;;
+        	-h|--help)
+            		USAGE
+            		exit 0
+            		;;
+        	*)
+            		echo "invalid parameter: $1"
+            		USAGE
+            		exit 1
+            		;;
+    	esac
+    	shift
+done
+
+# Executa as operações do Git se a mensagem foi fornecida pelo usuário
+if [ -n "$MESSAGE" ]; then
+    	git add .
+	echo "[*] Adding the changes to the local repository!"
+	echo
+    	git commit -m "$MESSAGE"
+	echo
+	echo "[*] Adding the commit!"
+	echo
+	# Caso o usuário esteja fazendo commit em outra branch, deve alterar a linha de comando para o nome da branch
+    	git push -u origin main
+	echo
+	echo "[*] The push was completed successfully!"
+else
+    	echo "[-] Error: The commit message is required with the -m | --message parameter"
+    	USAGE
+    	exit 1
+fi
